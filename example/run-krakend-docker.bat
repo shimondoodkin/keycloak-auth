65ce04c527dac87b3c19274ec73ccd052b064ade
Shimon Doodkin <helpmepro1@gmail.com>
2023-04-08 22:52:35 +0300

    update path

--- example/run-krakend-docker.bat
+++ example/run-krakend-docker.bat
@@ -1,1 +1,3 @@
-docker run --rm -it -p "8082:8080" --add-host host.docker.internal:host-gateway -v C:\Users\user\Documents\projects\kerberos\keycloak-auth\example\krakend.json:/etc/krakend/krakend.json devopsfaith/krakend
+SET CURRENT_FOLDER_NAME=%~dp0 && set CURRENT_FOLDER_NAME=%CURRENT_FOLDER_NAME:~0,-1%
+
+docker run --rm -it -p "8082:8080" --add-host host.docker.internal:host-gateway -v %CURRENT_FOLDER_NAME%\krakend.json:/etc/krakend/krakend.json devopsfaith/krakend
